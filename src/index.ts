46f54d0c93da7cd408ec2a41dcab94cf20661e27
READTalk <redtalk.app@hotmail.com>
2026-09-06 00:22:09 +0700

    Update light logo URL in index.ts

--- src/index.ts
+++ src/index.ts
@@ -131,7 +131,7 @@ export default {
 				favicon: "https://readtalk.pages.dev/favicon.ico",
 				logo: {
 					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
-					light: "https://readtalk.pages.dev/vite.svg",
+					light: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
 				},
 			},
 			success: async (ctx, value) => {
