e3a0eaec8c2744da953abc50d4b01d2d295e8d6e
READTalk <redtalk.app@hotmail.com>
2026-09-06 00:20:15 +0700

    Update logo URL for dark theme

--- src/index.ts
+++ src/index.ts
@@ -130,7 +130,7 @@ export default {
 				primary: "#000000",
 				favicon: "https://readtalk.pages.dev/favicon.ico",
 				logo: {
-					dark: "https://readtalk.pages.dev/vite.svg",
+					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
 					light: "https://readtalk.pages.dev/vite.svg",
 				},
 			},
